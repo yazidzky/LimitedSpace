2e725f4c353eb9480316972b722569f28c4f9b25
zinky <yazidzinky98@gmail.com>
2026-01-28 22:39:45 +0800

    fix level section

--- start_screen.gd
+++ start_screen.gd
@@ -4,21 +4,13 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
 	%StartButton.pressed.connect(_on_start_pressed)
-	%LevelSelectionButton.pressed.connect(_on_level_selection_pressed)
 	%CreditsButton.pressed.connect(_on_credits_pressed)
 	%ExitButton.pressed.connect(_on_exit_pressed)
 	%CloseCreditsButton.pressed.connect(_on_close_credits_pressed)
 
 func _on_start_pressed():
-	var gm = get_node_or_null("/root/GameManager")
-	var level_to_load = gm.levels[0] if gm else "res://Tutorial.tscn"
-	if has_node("/root/LoadingManager"):
-		get_node("/root/LoadingManager").load_level(level_to_load)
-	else:
-		get_tree().change_scene_to_file(level_to_load)
-
-func _on_level_selection_pressed():
 	get_tree().change_scene_to_file("res://level_selection.tscn")
+
 
 func _on_credits_pressed():
 	%CreditsPanel.visible = true
